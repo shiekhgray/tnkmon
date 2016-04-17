096008cca701457918d4788024d307d0a724a50d
shiekhgray <shiekhgray@gmail.com>
2016-04-17 19:30:17 -0400

    Adding a script to run every minute via crontab. It will querry the database and get the time and turn lights or co2 on or off as appropriate.

---
+++ tnkcron.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import os
+import sqlite3
+import datetime
+
+#connect to DB
+cwd = os.getcwd()
+conn = sqlite3.connect(cwd + '/tnkmon.db')
+
+time = datetime.datetime.now()
+hour_now = time.hour
+min_now = float(time.minute)
+min_percent = min_now / 60
+now = float(hour_now+min_percent)
+
+with conn:
+    ####Build cursor
+    cursor = conn.cursor()
+
+    ####Get lights information
+    cursor.execute("SELECT value FROM webctl WHERE key='light_on_start'")
+    light_on_start = cursor.fetchall()
+    cursor.execute("SELECT value FROM webctl WHERE key='light_on_dur'")
+    light_on_dur = cursor.fetchall()
+
+    light_start = light_on_start[0][0]
+    light_end = light_on_start[0][0] + light_on_dur[0][0]
+
+    light = "off"
+
+    if now >= light_start and now < light_end:
+        light = "on"
+
+    print light
+
+    ####Get CO2 information
+    cursor.execute("SELECT value FROM webctl WHERE key='co2_on_start'")
+    co2_on_start = cursor.fetchall()
+    cursor.execute("SELECT value FROM webctl WHERE key='co2_on_dur'")
+    co2_on_dur = cursor.fetchall()
+
+    co2_start = co2_on_start[0][0]
+    co2_end = co2_on_start[0][0] + co2_on_dur[0][0]
+
+    co2 = "off"
+
+    if now >= co2_start and now < co2_end:
+        co2 = "on"
+
+    print co2
