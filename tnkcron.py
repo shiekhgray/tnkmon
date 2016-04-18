f8e9bd153288e57267239c38dee6423428f824e4
shiekhgray <shiekhgray@gmail.com>
2016-04-17 20:13:21 -0400

    Refactor tnkcron so that we can call these functions from flask

--- tnkcron.py
+++ tnkcron.py
@@ -14,38 +14,44 @@ min_now = float(time.minute)
 min_percent = min_now / 60
 now = float(hour_now+min_percent)
 
-with conn:
-    ####Build cursor
-    cursor = conn.cursor()
+def get_light():
+    with conn:
+        ####Build cursor
+        cursor = conn.cursor()
 
-    ####Get lights information
-    cursor.execute("SELECT value FROM webctl WHERE key='light_on_start'")
-    light_on_start = cursor.fetchall()
-    cursor.execute("SELECT value FROM webctl WHERE key='light_on_dur'")
-    light_on_dur = cursor.fetchall()
+        ####Get lights information
+        cursor.execute("SELECT value FROM webctl WHERE key='light_on_start'")
+        light_on_start = cursor.fetchall()
+        cursor.execute("SELECT value FROM webctl WHERE key='light_on_dur'")
+        light_on_dur = cursor.fetchall()
 
-    light_start = light_on_start[0][0]
-    light_end = light_on_start[0][0] + light_on_dur[0][0]
+        light_start = light_on_start[0][0]
+        light_end = light_on_start[0][0] + light_on_dur[0][0]
 
-    light = "off"
+        light = "off"
 
-    if now >= light_start and now < light_end:
-        light = "on"
+        if now >= light_start and now < light_end:
+            light = "on"
 
-    print light
+    return light
 
-    ####Get CO2 information
-    cursor.execute("SELECT value FROM webctl WHERE key='co2_on_start'")
-    co2_on_start = cursor.fetchall()
-    cursor.execute("SELECT value FROM webctl WHERE key='co2_on_dur'")
-    co2_on_dur = cursor.fetchall()
+def get_co2():
+    with conn:
+        ####Build cursor
+        cursor = conn.cursor()
 
-    co2_start = co2_on_start[0][0]
-    co2_end = co2_on_start[0][0] + co2_on_dur[0][0]
+        ####Get CO2 information
+        cursor.execute("SELECT value FROM webctl WHERE key='co2_on_start'")
+        co2_on_start = cursor.fetchall()
+        cursor.execute("SELECT value FROM webctl WHERE key='co2_on_dur'")
+        co2_on_dur = cursor.fetchall()
 
-    co2 = "off"
+        co2_start = co2_on_start[0][0]
+        co2_end = co2_on_start[0][0] + co2_on_dur[0][0]
 
-    if now >= co2_start and now < co2_end:
-        co2 = "on"
+        co2 = "off"
 
-    print co2
+        if now >= co2_start and now < co2_end:
+            co2 = "on"
+
+    return co2
